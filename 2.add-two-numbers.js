aaac1cea3d60ced85a890876bc95c78b31841259
Xylight0 <greppn@gmail.com>
2022-03-16 22:12:38 +0100

    add-two-numbers start

---
+++ 2.add-two-numbers.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=2 lang=javascript
+ *
+ * [2] Add Two Numbers
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+  let node1 = l1;
+  let node2 = l2;
+  while (node1.next) {
+    console.log(node1.val);
+    node1 = node1.next;
+  }
+};
+// @lc code=end
